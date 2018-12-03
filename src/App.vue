61333ea2ed4556e3aa39265e112c2c8982b8d294
Kristin Hortsch <kristinhortsch@gmail.com>
2018-12-03 13:57:32 -0800

    got basic config files set up, Home page is up and rendering

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <header>
+      <img alt="justice league" src="./assets/justiceleague.jpg">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink></RouterLink>
+      </nav>
+    </header>
+
+    <main>
+      <RouterView></RouterView>
+    </main>
+
+  </div>
+</template>
+
+<script>
+
+export default {};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+header img {
+  height: 100%;
+}
+nav a {
+  text-decoration: none;
+  color: black;
+  margin: 3px;
+  padding: 3px;
+  border: 1px solid black;
+}
+main {
+  padding: 8px;
+}
+</style>
---
+++ src/components/home/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <section>
+    <h2>Welcome to Superheroes Search!</h2>
+    <router-link to="/people">Search for a Superhero:</router-link>
+  </section>
+</template>
+
+<style>
+</style>
