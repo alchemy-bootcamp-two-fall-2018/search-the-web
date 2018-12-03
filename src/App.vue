9ede11704c77e247acf909136156246b6c763d8e
Teonna Zaragoza <teonna.zaragoza@gmail.com>
2018-12-03 14:35:41 -0800

    created files and can render Home link from app

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+  <nav>
+    <RouterLink to="Home"> Home</RouterLink>
+  </nav>
+  <section>
+    <RouterView></RouterView>
+  </section>
+  </div>
+</template>
+
+<script>
+
+export default {
+   
+};
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
