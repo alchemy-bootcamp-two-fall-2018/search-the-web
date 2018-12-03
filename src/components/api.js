d9a39f2635d1d4dc0deba83452a786ba45d2ea69
Connor Frendt <cjacksonfx@yahoo.com>
2018-12-03 15:20:37 -0800

    initial files and main page rendering

---
+++ src/components/api.js
@@ -0,0 +1,6 @@
+export default {
+  getNews(searchTerm = '', page = 1) {
+    return fetch('https://newsapi.org/v2/everything?search=${encodeURIComponent(searchTerm)}&apiKey=fc1e1819ef4841e98ec5acff46998463');
+      .then(response => response.json());
+  }
+};
---
+++ src/components/home/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h2>Welcome to the News Search!</h2>
+    <router-link to="/news">Search News</router-link>
+  </section>
+</template>
+
+<style>
+
+</style>
