524abc674c12942f81fb97816f082745fb95c6af
Carmen V Ramos <carmenvramos@yahoo.com>
2018-12-04 06:17:32 -0800

    Fixed Travis Error

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ export default {
         // eslint-disable-next-line
       const apiKey = process.env.VUE_APP_API_KEY;
         // return fetch('https://newsapi.org/v2/everything?q=bush&apiKey=9c1283752a154aa9a9d72d01b0d8a83a')
-        return fetch(`https://newsapi.org/v2/everything?&apiKey=${apikey}&q=${encodeURIComponent(searchTerm)}`)
+        return fetch(`https://newsapi.org/v2/everything?&apiKey=${apiKey}&q=${encodeURIComponent(searchTerm)}`)
             .then(response => response.json());
     } 
 };
