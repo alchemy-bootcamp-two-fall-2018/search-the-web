907187c53e0eb3c8ab3e70f809bf6fc0c3c242bc
Carmen V Ramos <carmenvramos@yahoo.com>
2018-12-04 09:08:17 -0800

    Updated api.js url route

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,9 @@ export default {
     getNews(searchTerm = '') {
         // eslint-disable-next-line
       const apiKey = process.env.VUE_APP_API_KEY;
-        // return fetch('https://newsapi.org/v2/everything?q=bush&apiKey=9c1283752a154aa9a9d72d01b0d8a83a')
-        return fetch(`https://newsapi.org/v2/everything?&apiKey=${apiKey}&q=${encodeURIComponent(searchTerm)}`)
+        // return fetch('https://newsapi.org/v2/everything?https://newsapi.org/v2/everything?&apiKey=9c1283752a154aa9a9d72d01b0d8a83a')
+        // return fetch(`https://newsapi.org/v2/everything&apiKey=${apiKey}&q=${encodeURIComponent(searchTerm)}`)
+        return fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}&q=${encodeURIComponent(searchTerm)}`)
             .then(response => response.json());
     } 
 };
