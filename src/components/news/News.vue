4f59719aba98c3918cf4780002819cf5d97850ce
Connor Frendt <cjacksonfx@yahoo.com>
2018-12-03 16:00:58 -0800

    buttons and searching for p tag renders

--- src/components/news/News.vue
+++ src/components/news/News.vue
@@ -1,5 +1,17 @@
 <template>
-  <h2>NEWS</h2>
+  <section class="news">
+    <h2>News</h2>
+
+    <NewsSearch :onSearch="handleSearch" :search="search" />
+
+    <Loader :loading="loading" />
+
+    <p>
+      <button @click="handlePage(-1)" :disabled="page === 1">Prev</button>
+      Searching for &quot;{{ search }}&quot; - found {{total}} - page {{page}} of {{totalPages}}
+      <button @click="handlePage(1)" :disabled="totalPages === page">Next</button>
+    </p>
+  </section>
 </template>
 
 <script>
---
+++ src/components/news/NewsStory.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+    <RouterLink :to="`/news/${newsStory.id}`">
+    {{newsStory.name}} something something
+    </RouterLink>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
