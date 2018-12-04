f69af8ecb7284952e8b02491bbd1bfb139650900
‘Michael <pickup.patterns@gmail.com>
2018-12-03 16:48:05 -0800

    added discogs api for music collection user

--- services/api.js
+++ services/api.js
@@ -1,3 +1,6 @@
 export default {
-    get
-}
+  getRecords(searchTerm = '') {
+    return fetch ('http://api.discogs.com/users/pickuppatterns/collection/folders/0/search=${encodeURIComponent(searchTerm)}&page=${page}')
+      .then(response => response.json());
+  }
+};
