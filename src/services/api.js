6975cb3a642f1c5f0fa667bb7fc6664c8d73a915
Abel Quintero <abel.j.quintero@gmail.com>
2018-12-06 09:58:34 -0800

    made changes based on Ryan's feedback

--- src/services/api.js
+++ src/services/api.js
@@ -6,9 +6,9 @@ export default {
 
     if(searchTerm.startsWith('fake')) {
       return Promise.reject(new Error('FAKE NEWS'));
-    };
+    }
 
-    return fetch('https://newsapi.org/v2/everything?q=Apple&from=2018-12-03&sortBy=popularity&apiKey=2ca2954860044284aefd2b4c3dd9d2d8').then(response => response.json());
+    return fetch(`https://newsapi.org/v2/everything?q=${searchTerm}&from=2018-12-03&sortBy=popularity&apiKey=2ca2954860044284aefd2b4c3dd9d2d8`).then(response => response.json());
   
   }
 
