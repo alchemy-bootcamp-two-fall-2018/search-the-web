95bdc5bbd00e54b39ba79a39a42d1eb19df9dc5b
avanvalk <drewvv@gmail.com>
2018-12-11 21:11:33 -0800

    setup the api properly

--- src/components/services/api.js
+++ src/components/services/api.js
@@ -1,10 +1,10 @@
-var url = 'https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
-          'from=2018-12-03&' +
-          'sortBy=popularity&' +
-          'apiKey=249ab959bc50472b8f9bc61d6919eb9e';
-var req = new Request(url);
-fetch(req)
-  .then(function(response) {
-    console.log(response.json());
-  });
+
+export default {
+  getNews(searchTerm = '') {
+
+    // eslint-disable-next-line
+    return fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${APIKEY}&q=${encodeURIComponent(searchTerm)}`)
+  
+      .then(response => response.json());
+  }
+};
