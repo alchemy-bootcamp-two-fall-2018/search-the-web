d64f76cb9ab3eeed85aa0fccb49a52b9cb40d0b8
avanvalk <drewvv@gmail.com>
2018-12-11 20:43:20 -0800

    added News.vue

---
+++ src/components/news/News.vue
@@ -0,0 +1,15 @@
+
+<template>
+    <li v-if="news">
+        <a :href="news.url" target="_blank"><h4>{{news.title}}</h4></a>
+        <p>{{news.description}}</p>
+    </li>
+</template>
+
+<script>
+export default {
+  props: {
+    news: Object
+  }
+};
+</script>
